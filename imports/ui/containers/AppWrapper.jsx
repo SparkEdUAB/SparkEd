fb418aef731e9f4e2c792ba5d02836381f9edc80
Olivier JM <manolivier93@gmail.com>
2018-05-28 11:09:09 +0200

    updated to react 16.3

--- imports/ui/containers/AppWrapper.jsx
+++ imports/ui/containers/AppWrapper.jsx
@@ -4,18 +4,39 @@
 import React, { Component, Fragment } from 'react';
 import { PropTypes } from 'prop-types';
 import Header from '../components/layouts/Header';
+import { ThemeContext } from './ThemeContext';
 
-export default class AppWrapper extends Component {
-  render() {
-    return (
-      <Fragment>
+// export default class AppWrapper extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <ThemeContext.Consumer>
+//         {theme => (
+//           <Fragment {...props}>
+//             <Header />
+//             <Fragment>{this.props.children}</Fragment>
+//           </Fragment>
+//         )}
+//       </ThemeContext.Consumer>
+//     );
+//   }
+// }
+
+const AppWrapper = props => (
+  <ThemeContext.Provider>
+    {theme => (
+      <Fragment {...props} style={{ backgroundColor: theme.background }}>
         <Header />
         <Fragment>{this.props.children}</Fragment>
       </Fragment>
-    );
-  }
-}
+    )}
+  </ThemeContext.Provider>
+);
 
 AppWrapper.propTypes = {
   children: PropTypes.node.isRequired,
 };
+
+export default AppWrapper;
