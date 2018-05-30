db983fed63b947efb2b583800f288a738b1e8941
Olivier JM <manolivier93@gmail.com>
2018-05-30 11:24:24 +0200

    changed Video.jsx to a pure component

--- imports/ui/components/content/Video.jsx
+++ imports/ui/components/content/Video.jsx
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { PropTypes } from 'prop-types';
 
-export default class Video extends Component {
-  render() {
-    const { link } = this.props;
-    return (
-      <div className="videoViewerContainer">
-        <video controls autoPlay className="videoViewer">
-          <source src={link} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-    );
-  }
-}
+const Video = props => (
+  <div className="videoViewerContainer">
+    <video controls autoPlay className="videoViewer">
+      <source src={props.link} type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  </div>
+);
 
 Video.propTypes = {
   link: PropTypes.string.isRequired,
 };
+
+export default Video;
