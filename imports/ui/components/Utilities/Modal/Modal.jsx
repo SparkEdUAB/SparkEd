3a4f14a09935d735281e8a192ccf8d14f75f8fc0
Olivier JM <manolivier93@gmail.com>
2018-08-07 20:58:51 +0200

    changed the EditAccounts component into functional component

--- imports/ui/components/Utilities/Modal/Modal.jsx
+++ imports/ui/components/Utilities/Modal/Modal.jsx
@@ -4,31 +4,41 @@ import { PropTypes } from 'prop-types';
 export class ModalDialog extends Component {
   render() {
     return (
-          <div id={this.props.id} className="modal">
-            <div ref='modal_upload' className="modal-content">
-              <h4>{this.props.title}</h4>
-              <p>{this.props.msg}</p>
-            </div>
-            <div className="modal-footer">
-            <a href="#" id="closeModal" className="modal-action modal-close  waves-effect waves-green btn-flat" style={{ marginRight: 20 }}>No</a>
-            <a href="#" onClick={this.props.callBack} id="yes" className="modal-action  waves-effect waves-green btn-flat">Yes</a>
-
-          </div>
-          </div>
+      <div id={this.props.id} className="modal">
+        <div ref="modal_upload" className="modal-content">
+          <h4>{this.props.title}</h4>
+          <p>{this.props.msg}</p>
+        </div>
+        <div className="modal-footer">
+          <a
+            href="#"
+            id="closeModal"
+            className="modal-action modal-close  waves-effect waves-green btn-flat"
+            style={{ marginRight: 20 }}
+          >
+            No
+          </a>
+          <a
+            href="#"
+            onClick={this.props.callBack}
+            id="yes"
+            className="modal-action  waves-effect waves-green btn-flat"
+          >
+            Yes
+          </a>
+        </div>
+      </div>
     );
   }
 }
 
 // propTypes
 ModalDialog.propTypes = {
-
   msg: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   callBack: PropTypes.func.isRequired,
 };
-
-
 
 // functions
 
