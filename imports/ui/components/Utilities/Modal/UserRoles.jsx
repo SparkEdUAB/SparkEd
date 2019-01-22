f85029167edaf6a74020b0f1f248510c8ad203e8
Olivier JM <manolivier93@gmail.com>
2019-01-22 13:24:52 +0200

    fixed the problem in initializing the select

--- imports/ui/components/Utilities/Modal/UserRoles.jsx
+++ imports/ui/components/Utilities/Modal/UserRoles.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import M from 'materialize-css';
 
 export class UserRoles extends React.Component {
   componentDidMount() {
-    $('select').material_select();
+    M.AutoInit();
   }
   render() {
     return (
-      <select id="auth" required="true" defaultValue={this.props.value} name="roles">
+      <select id="auth" required defaultValue={this.props.value} name="roles">
         <option value="admin">Admin</option>
         <option value="content-manager">Content-Manager</option>
         <option value="student">Student</option>
