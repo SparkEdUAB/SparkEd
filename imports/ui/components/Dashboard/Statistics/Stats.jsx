b96c661a79a44a1f2eb9ec567bd3e55b1566bb26
Olivier JM <manolivier93@gmail.com>
2019-02-05 13:22:14 +0200

    added resources and references count

--- imports/ui/components/Dashboard/Statistics/Stats.jsx
+++ imports/ui/components/Dashboard/Statistics/Stats.jsx
@@ -3,13 +3,18 @@ import { Meteor } from 'meteor/meteor';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import { _Courses } from '../../../../api/courses/courses';
+import { _Units } from '../../../../api/units/units';
+import { _Topics } from '../../../../api/topics/topics';
+import { Resources, References } from '../../../../api/resources/resources';
 
-export function StatsView({ users, courses }) {
+export function StatsView({ users, courses, units, topics, resources }) {
   return (
     <div className="col m9 s11">
       <StatCard count={users} type={'users'} />
       <StatCard count={courses} type={'courses'} />
-      <StatCard count={2} type={'units'} />
+      <StatCard count={units} type={'units'} />
+      <StatCard count={topics} type={'topics'} />
+      <StatCard count={resources} type={'resources'} />
     </div>
   );
 }
@@ -34,17 +39,23 @@ StatCard.propTypes = {
 StatsView.propTypes = {
   users: PropTypes.number.isRequired,
   courses: PropTypes.number.isRequired,
+  units: PropTypes.number.isRequired,
+  topics: PropTypes.number.isRequired,
+  resources: PropTypes.number.isRequired,
 };
 
 export default withTracker(() => {
   Meteor.subscribe('users');
   Meteor.subscribe('courses');
-  Meteor.subscribe('users');
+  Meteor.subscribe('units');
+  Meteor.subscribe('topics');
+  Meteor.subscribe('resourcess');
   return {
     users: Meteor.users.find().count(),
     courses: _Courses.find().count(),
-    topics: [],
-    resources: [],
+    topics: _Topics.find().count(),
+    units: _Units.find().count(),
+    resources: Resources.find().count(),
   };
 })(StatsView);
 // export default StatsView;
