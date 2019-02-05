3cd3f780e6002286376fdf417cc7c50b3e0277b1
Olivier JM <manolivier93@gmail.com>
2019-02-05 13:08:55 +0200

    getting the total number of users

--- imports/ui/components/Dashboard/Statistics/Stats.jsx
+++ imports/ui/components/Dashboard/Statistics/Stats.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import PropTypes from 'prop-types';
+import { withTracker } from 'meteor/react-meteor-data';
 
-export function Stats() {
+export function StatsView({ users }) {
   return (
     <div className="col m9 s11">
-      <StatView />
-      <StatView />
-      <StatView />
+      <StatCard count={users} />
+      <StatCard count={2} />
+      <StatCard count={2} />
     </div>
   );
 }
 
-function StatView() {
+function StatCard({ count }) {
   return (
     <div className="col s6 m4">
       <div className="card-panel teal">
-        <h1 className="white-text center-align">100</h1>
+        <h1 className="white-text center-align">{count}</h1>
         <br />
         <h5 className="white-text center-align">users</h5>
       </div>
@@ -22,4 +25,21 @@ function StatView() {
   );
 }
 
-export default Stats;
+StatCard.propTypes = {
+  count: PropTypes.number.isRequired,
+};
+
+StatsView.propTypes = {
+  users: PropTypes.number.isRequired,
+};
+
+export default withTracker(() => {
+  Meteor.subscribe('users');
+  return {
+    users: Meteor.users.find().count(),
+    courses: [],
+    topics: [],
+    resources: [],
+  };
+})(StatsView);
+// export default StatsView;
