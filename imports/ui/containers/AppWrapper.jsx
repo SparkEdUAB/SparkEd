6141c48d8ca9737199ab7151b2b6e79f76d09a04
Olivier JM <manolivier93@gmail.com>
2019-02-06 19:53:55 +0200

    removed unused code

--- imports/ui/containers/AppWrapper.jsx
+++ imports/ui/containers/AppWrapper.jsx
@@ -1,22 +1,12 @@
 /* eslint class-methods-use-this: "off" */
 /* eslint import/no-unresolved: "off" */
-
 import React, { Fragment } from 'react';
 import { PropTypes } from 'prop-types';
-import { withTracker } from 'meteor/react-meteor-data';
-// import { Session } from 'meteor/session';
-import { _Settings } from '../../api/settings/settings';
 import Header from '../components/layouts/Header';
 
 export const ThemeContext = React.createContext();
 
-// const colors = {
-//   main: Session.get('main'),
-//   isDark: Session.get('isDark'),
-//   mainDark: Session.get('main'),
-//   bodyBackground: '',
-// };
-export class AppWrapper extends React.Component {
+export default class AppWrapper extends React.Component {
   state = {
     isDark: false,
     mainDark: '#212121',
@@ -45,25 +35,8 @@ export class AppWrapper extends React.Component {
     );
   }
 }
-// export const AppWrapper = ({ children, color }) => {
-// if (!color) {
-//   // color = colors; //eslint-disable-line
-//   return 'loading';
-// }
-// return (
-//   <ThemeContext.Provider value={color}>
-//     <div style={{ backgroundColor: color.isDark ? '#252829' : '#fff' }}>
-//       <Header />
-//       <Fragment>{children}</Fragment>
-//     </div>
-//   </ThemeContext.Provider>
-// );
-// };
+
 AppWrapper.propTypes = {
   children: PropTypes.node.isRequired,
   color: PropTypes.object,
 };
-
-export default withTracker(() => ({
-  color: _Settings.findOne({}), // get the current main color
-}))(AppWrapper);
