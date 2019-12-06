a884c28b2799ee6ef754ebb7acbb8647a06bf621
OlivierJM <manolivier93@gmail.com>
2019-12-06 08:25:25 +0200

    fixed the eslint error

--- server/init.js
+++ server/init.js
@@ -1,6 +1,6 @@
 /* eslint no-console: 'off' */
-import { Meteor } from "meteor/meteor";
-import fs from "fs-extra";
+import { Meteor } from 'meteor/meteor';
+import fs from 'fs-extra';
 
 Meteor.startup(() => {
   const currentPath = `${process.env.PWD}/public/sparked.png`;
@@ -8,10 +8,10 @@ Meteor.startup(() => {
   try {
     if (fs.statSync(currentPath)) {
       fs.copy(currentPath, newPath)
-        .then(() => console.log(""))
+        .then(() => console.log(''))
         .catch(err => console.error(err));
     } else {
-      console.log("no such file exists!");
+      console.log('no such file exists!');
     }
   } catch (err) {
     console.error(err);
