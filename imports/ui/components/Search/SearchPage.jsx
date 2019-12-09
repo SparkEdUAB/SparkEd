e3a9f17cd1250cfba5f8592c6a64c4c0474e02dc
OlivierJM <manolivier93@gmail.com>
2019-12-09 23:40:44 +0200

    linked and beautified the search page

--- imports/ui/components/Search/SearchPage.jsx
+++ imports/ui/components/Search/SearchPage.jsx
@@ -6,7 +6,11 @@ export default function SearchPage() {
         <div className="search-page">
             <div className="row">
                 <div className="input-field col s12">
-                    <input placeholder="Search here" type="text" className="validate" />
+                    <input style={{
+                        fontSize: '1.8em',
+                        color: '#838383',
+                        padding: 20,
+                    }} placeholder="Search here" type="text" className="validate" autoFocus />
                 </div>
             </div>
         </div>
